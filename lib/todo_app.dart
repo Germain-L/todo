3e08479605734b94257e6d827c037089a045365e
Germain-L <germain.leignel@gmail.com>
2020-08-20 19:14:30 +0200

    made some progress

--- lib/todo_app.dart
+++ lib/todo_app.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'UI/all.dart';
+import 'UI/new_task_bar.dart';
 
 class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: Scaffold(
-        body: AllTasks()
+      home: SafeArea(
+        child: Scaffold(
+          appBar: NewTaskAppBar(),
+          body: AllTasks()
+        ),
       ),
     );
   }
